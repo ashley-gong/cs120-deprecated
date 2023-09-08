d3257b0522583ddbdef9b0787c91592f1c30b023
ashley-gong <ashley.q.gong@gmail.com>
2023-09-08 00:13:50 -0400

    1a

--- fall2023/psets/ps0/ps0.py
+++ fall2023/psets/ps0/ps0.py
@@ -30,8 +30,18 @@ class BTvertex:
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
 def calculate_sizes(v):
-    # Your code goes here
-    pass
+    # Set each node size to 1 to begin with
+    v.size = 1
+    # Base case is a leaf node - simply return size if no children
+    if not v.left and not v.right:
+        return v.size
+    # Add current node to size calculation
+    if v.left:
+        v.size += calculate_sizes(v.left)
+    if v.right:
+        v.size += calculate_sizes(v.right)
+    
+    return v.size
 
 #
 # Problem 1c
