4323eb4fc20745c152f8dd502fb08f805de7e26d
ashley-gong <ashley.q.gong@gmail.com>
2023-09-09 11:28:07 -0400

    1a alternate method

--- fall2023/psets/ps0/ps0.py
+++ fall2023/psets/ps0/ps0.py
@@ -11,7 +11,8 @@
 class BinaryTree:
     def __init__(self, root):
         self.root: BTvertex = root
- 
+
+
 class BTvertex:
     def __init__(self, key):
         self.parent: BTvertex = None
@@ -20,9 +21,11 @@ class BTvertex:
         self.key: int = key
         self.size: int = None
 
+
 #
 # Problem 1a
 #
+
 
 # Input: BTvertex v, the root of a BinaryTree of size n
 # Output: Up to you
@@ -30,28 +33,23 @@ class BTvertex:
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
 def calculate_sizes(v):
-    # Set each node size to 1 to begin with
-    v.size = 1
-    # Base case is a leaf node - simply return size if no children
-    if not v.left and not v.right:
-        return v.size
-    # Add current node to size calculation
-    if v.left:
-        v.size += calculate_sizes(v.left)
-    if v.right:
-        v.size += calculate_sizes(v.right)
-    
+    # Base case: when node doesn't exist, don't increment size
+    if not v:
+        return 0
+    v.size = 1 + calculate_sizes(v.left) + calculate_sizes(v.right)
     return v.size
+
 
 #
 # Problem 1c
 #
+
 
 # Input: BTvertex r, the root of a size-augmented BinaryTree T
 # ... of size n and height h
 # Output: A BTvertex that, if removed from the tree, would result
 # ... in disjoint trees that all have at most n/2 vertices
 # Runtime: O(h)
-def find_vertex(r): 
+def find_vertex(r):
     # Your code goes here
     pass
